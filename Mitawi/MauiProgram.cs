23910681404219ac5f9811d641026b64123b22eb
Daniel Monettelli <danielmonetelli@hotmail.com>
2022-08-28 10:50:43 -0500

    ⬆️ [Code] Added "Sentry.Maui" v3.20.1-preview.2

--- Mitawi/MauiProgram.cs
+++ Mitawi/MauiProgram.cs
@@ -6,13 +6,21 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        builder.UseMauiApp<App>()
+        .UseSentry(options =>
+        {
+            options.Dsn = "INSERT_DSN_SENTRY_HERE";
+            options.Debug = true;
+            options.TracesSampleRate = 1.0;
+        })
+        .ConfigureFonts(fonts =>
         {
             fonts.AddFont("Roboto-Bold.ttf", "Roboto#700");
             fonts.AddFont("Roboto-Medium.ttf", "Roboto#500");
             fonts.AddFont("Roboto-Regular.ttf", "Roboto#400");
             fonts.AddFont("customfonticons.ttf", "CustomFontIcons");
         }).UseMauiCommunityToolkit();
+
 
         return builder.Build();
     }
