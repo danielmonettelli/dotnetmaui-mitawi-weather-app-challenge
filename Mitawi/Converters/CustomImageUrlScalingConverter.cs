78f59c8947660100bf065794f62b5efd2168c62a
Daniel Monettelli <danielmonetelli@hotmail.com>
2022-09-17 19:07:40 -0500

    🚀 [Code] Changed path of CustomImageUrlScalingConverter.cs s1

--- Mitawi/Converters/CustomImageUrlScalingConverter.cs
+++ Mitawi/Converters/CustomImageUrlScalingConverter.cs
@@ -8,7 +8,7 @@ namespace Mitawi.Converters
         public override object ConvertFrom(object value, object parameter, CultureInfo culture)
         {
             // Image scale type in the parameter: 2(100x100approx), 4(200x200approx), 10(500x500approx)
-            return "https://raw.githubusercontent.com/danimonettelli/MyResources/main/OpenWeather_Icons_Redesign/" + value + "@" + parameter + "x.png";
+            return "https://raw.githubusercontent.com/danielmonettelli/MyResources/main/OpenWeather_Icons_Redesign/" + value + "@" + parameter + "x.png";
         }
 
         public override object ConvertBackTo(object value, object parameter, CultureInfo culture)
