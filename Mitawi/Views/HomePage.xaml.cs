34285b77bc5ba1ca3c9bfff09c6c745cfe8a6dcd
Daniel Monettelli <danielmonetelli@hotmail.com>
2022-10-01 22:14:31 -0500

    🚀 [Code] Added "location permissions" in code behind

--- Mitawi/Views/HomePage.xaml.cs
+++ Mitawi/Views/HomePage.xaml.cs
@@ -11,8 +11,39 @@ namespace Mitawi.Views
             InitializeComponent();
 
             SizeChanged += HomePage_Orientation;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await CheckAndRequestLocationPermission();
 
             BindingContext = ViewModelLocator.HomeViewModel;
+        }
+
+        private static async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                // Prompt the user to turn on in settings
+                // On iOS once a permission has been denied it may not be requested again from the application
+                return status;
+            }
+
+            if (Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>())
+            {
+                // Prompt the user with additional information as to why the permission is needed
+            }
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+            return status;
         }
 
         private void HomePage_Orientation(object sender, EventArgs e)
